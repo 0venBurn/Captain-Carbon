e8b43d1a09e97b9fee3d969f2f92e7b22398bb74
Evan <evanbyrnecode@gmail.com>
2024-03-14 23:03:31 +0000

    added code for main menu and game logic

--- Game/core/src/MainMenuScreen.java
+++ Game/core/src/MainMenuScreen.java
@@ -3,15 +3,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import jdk.tools.jmod.Main;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class MainMenuScreen implements Screen{
+    private OurGame game;
     private SpriteBatch batch;
     private BitmapFont font;
+    private Stage stage;
+    private TextButton playButton;
+    private TextButton quitButton;
 
-    public MainMenuScreen(){
+    public MainMenuScreen(OurGame game){
+        this.game = game;
         batch = new SpriteBatch();
         font = new BitmapFont();
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage); // Set the stage to process input
+
+//      Create a TextButton Style
+
+
+
     }
 
     @Override
@@ -22,6 +35,39 @@ public class MainMenuScreen implements Screen{
     @Override
     public void render(float delta){
         Gdx.gl.glClearColor(0,0,0,1); // Black Background
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        font.draw(batch, "Press to Play!", (float) Gdx.graphics.getWidth() / 2, (float) Gdx.graphics.getHeight() / 2);
+
+        if(Gdx.input.justTouched()){
+//            Change to the game screen
+        }
+    }
+    @Override
+    public void resize(int width, int height){
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+
     }
 }
