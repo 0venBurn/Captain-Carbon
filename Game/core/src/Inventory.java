bdff12f6dc2ed3346ae10fe9eb1a2e63f06854f4
Evan <evanbyrnecode@gmail.com>
2024-03-17 20:29:22 +0000

    Game screen and pause screen interface added

--- Game/core/src/Inventory.java
+++
@@ -1,9 +0,0 @@
-public class Inventory {
-    public void addItem(Item item) {
-        // Adds an item to the inventory
-    }
-
-    public void removeItem(Item item) {
-        // Removes an item from the inventory
-    }
-}
