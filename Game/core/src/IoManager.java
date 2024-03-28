7bb86d8a4db6a25af1addf12cc58f9a0f86fcc60
Carlo <carlofinnegan2000@gmail.com>
2024-03-28 18:44:12 +0000

    updates

---
+++ Game/core/src/IoManager.java
@@ -0,0 +1,3 @@
+public class IoManager {
+    // Implement input/output management methods here
+}
---
+++ Game/core/src/Trip.java
@@ -0,0 +1,22 @@
+public class Trip {
+    // Trip related attributes like source, destination, etc.
+
+
+
+    public void calculateCarbonFootprint() {
+        // Calculation logic for carbon footprint
+
+    }
+
+    public void setSource() {
+        // Calculation logic for carbon footprint
+
+    }
+
+
+    public void setDestination() {
+        // Calculation logic for carbon footprint
+
+    }
+
+}
