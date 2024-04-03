452b94ebf672ffe60113035d5727e6f8412821ab
0venburn <evanbyrnecode@gmail.com>
2024-04-03 18:53:53 +0100

    Tutorial setup

--- Game/core/src/com/mygdx/game/MainMenuScreen.java
+++ Game/core/src/com/mygdx/game/MainMenuScreen.java
@@ -65,7 +65,7 @@ public class MainMenuScreen implements Screen {
         playButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameScreen());
+                game.setScreen(new GameScreen(game));
             }
         });
 
