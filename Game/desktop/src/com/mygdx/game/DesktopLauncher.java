97e7bfce9752fdfc24d5c34081bf39395cbe4661
Evan <evanbyrnecode@gmail.com>
2024-06-08 23:08:52 +0100

    added readme for open sourcing

--- Game/desktop/src/com/mygdx/game/DesktopLauncher.java
+++ Game/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Ride Sharer");
+		config.setTitle("Captain Carbon");
 //		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 
 		 config.setWindowedMode(1920, 1280); // Uncomment this line to switch to windowed mode
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# wolves.txt
+# Captain Carbon
+
+Education Carbon Awareness game created by 
+
+Mark Tynan, Evan Byrne, Carlo Finnegan, Zack Istasse, Ivan Kalgashkin
+
